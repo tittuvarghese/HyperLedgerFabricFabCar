2ef3059c501dcbc85a7aea0d83bf4ca687642da0
Tittu Varghese <tittu.parcx@gmail.com>
2018-02-12 15:45:59 +0530

    Minor updates

--- blockchain/query.go
+++ blockchain/query.go
@@ -30,3 +30,27 @@ func (setup *FabricSetup) QueryAll() (string, error) {
 	}
 	return string(transactionProposalResponses[0].ProposalResponse.GetResponse().Payload), nil
 }
+
+//QueryOne - Retrieves a single record by accepting Key
+func (setup *FabricSetup) QueryOne(value string) (string, string, error) {
+
+	// Prepare arguments
+	var args []string
+	args = append(args, "invoke")
+	args = append(args, "queryone")
+	args = append(args, value)
+
+	// Make the proposal and submit it to the network (via our primary peer)
+	transactionProposalResponses, txID, err := fcutil.CreateAndSendTransactionProposal(
+		setup.Channel,
+		setup.ChaincodeId,
+		setup.ChannelId,
+		args,
+		[]api.Peer{setup.Channel.GetPrimaryPeer()}, // Peer contacted when submitted the proposal
+		nil,
+	)
+	if err != nil {
+		return "", "", fmt.Errorf("Create and send transaction proposal return error in the query one record: %v", err)
+	}
+	return string(transactionProposalResponses[0].ProposalResponse.GetResponse().Payload), txID, nil
+}
